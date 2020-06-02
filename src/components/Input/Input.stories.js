51cc4410334bb0c6a9106bfeca2bf14abdd151bf
Simon <simon.nordstrom@chasacademy.se>
2020-06-02 16:17:25 +0200

    add attribute to inputs in Input.stories.js

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -5,6 +5,7 @@ import Input from './Input';
 
 export default { title: 'Input' };
 
+// Styling for storybook
 const StorybookWhiteCentered = {
   display: 'flex',
   flexDirection: 'column',
@@ -21,6 +22,7 @@ const normalmode = {
   alignItems: 'center',
   justifyContent: 'center',
 };
+
 const darkmode = {
   paddingTop: '20px',
   backgroundColor: `${Colors.DarkBlue}`,
@@ -34,6 +36,7 @@ const margin = {
   marginBottom: '20px',
   marginTop: '10px',
 };
+// End styling for storybook
 
 const StorybookInput = styled(Input)`
   ::placeholder {
@@ -46,15 +49,30 @@ export const NormalInput = () => {
     <div>
       <div style={(StorybookWhiteCentered, normalmode)}>
         <p>Normal mode</p>
-        <StorybookInput style={margin} placeholder="Adress" />
-        <StorybookInput style={margin} placeholder="Name" />
-        <StorybookInput style={margin} placeholder="Password" />
+        <StorybookInput type="text" style={margin} placeholder="Adress" />
+        <StorybookInput type="text" style={margin} placeholder="Name" />
+        <StorybookInput type="password" style={margin} placeholder="Password" />
       </div>
       <div style={darkmode}>
         <p>Dark</p>
-        <StorybookInput darkmode style={margin} placeholder="Adress" />
-        <StorybookInput darkmode style={margin} placeholder="Name" />
-        <StorybookInput darkmode style={margin} placeholder="Password" />
+        <StorybookInput
+          type="text"
+          darkmode
+          style={margin}
+          placeholder="Adress"
+        />
+        <StorybookInput
+          type="text"
+          darkmode
+          style={margin}
+          placeholder="Name"
+        />
+        <StorybookInput
+          type="password"
+          darkmode
+          style={margin}
+          placeholder="Password"
+        />
       </div>
     </div>
   );
