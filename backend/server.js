ff1a323dcc8394661102405a1b5429e8a69ae616
Andreas Ny <andreas.ny@chasacademy.se>
2020-06-02 19:44:47 +0200

    Insert user with first- and lastname into the database.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-async function run() {
+async function getAllUsers() {
   try {
     await client.connect();
     console.log('Connected correctly to server');
@@ -22,6 +22,7 @@ async function run() {
     const col = db.collection('users');
     const allUsers = await col.find({}).toArray();
     // Print to the console
+    console.log('getAllUsers:');
     console.log(allUsers);
   } catch (err) {
     console.log(err.stack);
@@ -30,4 +31,28 @@ async function run() {
   }
 }
 
-run().catch(console.dir);
+async function insertUser(firstName, lastName) {
+  const user = {
+    firstName,
+    lastName,
+  };
+  try {
+    await client.connect();
+    console.log('Connected correctly to server');
+    const db = client.db(dbName);
+    // Use the collection "users"
+    const col = db.collection('users');
+    // Insert user in collection
+    const insertResult = await col.insertOne(user);
+    console.log('Inserted user: ');
+    console.log(insertResult.ops);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+insertUser('Test', 'User').catch(console.dir);
+
+// getAllUsers().catch(console.dir);
