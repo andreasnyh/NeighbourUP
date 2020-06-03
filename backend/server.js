2e7671045b30ce25e0fc30544ed4758d629340e8
Andreas Ny <andreas.ny@chasacademy.se>
2020-06-03 11:49:08 +0200

    Add db connection with mongoose. Add user function

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+// const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbUser = process.env.REACT_APP_DBUSER;
+const dbPass = process.env.REACT_APP_DBPASS;
+const dbName = process.env.REACT_APP_DBNAME;
+const dbHost = process.env.REACT_APP_DBHOST;
+
+const uri = `mongodb+srv://${dbUser}:${dbPass}@${dbHost}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+// Do we need this??
+// mongoose.Promise = global.Promise;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // we're connected!
+  console.log('connected!');
+});
+
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema, 'users');
+const addUser = new User({
+  firstName: 'Mongoose User',
+  lastName: 'With model&schema',
+  email: 'mon@goose.com',
+});
+
+addUser.save((err, doc) => {
+  if (err) return console.log(err);
+
+  return console.log('User added: \n', doc);
+});
