3d69f2e6e09e2fee18f4bc1b965035e6f69bc8bb
Andreas Ny <andreas.ny@chasacademy.se>
2020-06-04 16:40:31 +0200

    One working test with mocha
    Add dependencies:
      - chai
      - mongodb-memory-server
      - supertest
    
    Refactor server.js -> make connect() and close()
    and make a fake database for testing

---
+++ backend/test/api/users/get.js
@@ -0,0 +1,34 @@
+process.env.NODE_ENV = 'test';
+
+const { expect } = require('chai');
+const { after, describe, before, it } = require('mocha');
+const request = require('supertest');
+
+// const app = require('../../../routes/user');
+const server = require('../../../server');
+
+describe('GET /user', () => {
+  before((done) => {
+    server
+      .connect()
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+
+  after((done) => {
+    server
+      .close()
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+  it('OK, getting users has no users', (done) => {
+    request(server.app)
+      .get('/user')
+      .then((res) => {
+        console.log(res.body);
+        expect(res.body.length).to.equal(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
