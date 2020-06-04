6c6c178744523eeb635bea8c1cc225d19d80966c
Per Egertun <per.egertun@chasacademy.se>
2020-06-04 13:04:02 +0200

    reset app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import StyledButton from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Sign in</p>
-        <StyledButton>Light Mode</StyledButton>
-        {/* <Button darkMode>Dark Mode</Button> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
