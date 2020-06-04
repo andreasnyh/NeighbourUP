b8f8e37aacf8c4d5f8b6ec0e3be49b3dd9c5ef04
Andreas Ny <andreas.ny@chasacademy.se>
2020-06-04 16:40:31 +0200

    One working test with mocha
    Add dependencies:
      - chai
      - mongodb-memory-server
      - supertest
    
    Refactor server.js -> make connect() and close()
    and make a fake database for testing

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 
+// Testing
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 // const User = require('./models/user.model');
+const uri = process.env.ATLAS_URI;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,17 +17,50 @@ app.use(cors());
 app.use(express.json());
 
 // Connection to MongoDB
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
+async function connect() {
+  if (process.env.NODE_ENV === 'test') {
+    const mongoServer = new MongoMemoryServer();
+    mongoServer.getUri().then((mongoUri) => {
+      mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      });
+      mongoose.connection.on(
+        'error',
+        console.error.bind(console, 'connection error:')
+      );
+      mongoose.connection.once('open', () => {
+        console.log(
+          'Fake database connection established successfully \n @',
+          mongoUri
+        );
+      });
+    });
+  } else {
+    // return new Promise((resolve, reject) => {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    // .then((res, err) => {
+    //   if (err) return reject(err);
+    //   resolve();
+    // });
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+      console.log('MongoDB database connection established successfully');
+    });
+    // });
+  }
+}
+
+function close() {
+  return mongoose.disconnect();
+}
 
 // Router
 const userRouter = require('./routes/user');
@@ -31,6 +68,10 @@ const userRouter = require('./routes/user');
 // Routes
 app.use('/user', userRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+connect().then(
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  })
+);
+
+module.exports = { connect, close, app };
