3fe596aea2fb9f2f94b51fe186856c243f201797
Andreas Ny <andreas.ny@chasacademy.se>
2020-06-08 11:29:36 +0200

    WIP post test

--- backend/test/api/users/get.js
+++ backend/test/api/users/get.js
@@ -19,7 +19,7 @@ describe('GET /user', () => {
       .then(() => done())
       .catch((err) => done(err));
   });
-  it('OK, getting users has no users', (done) => {
+  it('No users in database', (done) => {
     request(app)
       .get('/')
       .then((res) => {
@@ -29,4 +29,24 @@ describe('GET /user', () => {
       })
       .catch((err) => done(err));
   });
+  /* it('Add one user and check if saved', (done) => {
+    request(app)
+      .post('/add')
+      .send({
+        firstName: 'USER FIRST',
+        lastName: 'USER LAST',
+        email: 'USER EMAIL',
+      })
+      .expect(200)
+      .then(() => {
+        request(app)
+          .get('/')
+          .then((res) => {
+            console.log(res.body);
+            expect(res.body.length).to.equal(1);
+            done();
+          });
+      })
+      .catch((err) => done(err));
+  }); */
 });
