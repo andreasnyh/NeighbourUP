349b564dd4ead7842687471c48e41d622c59b0a3
Andreas Ny <andreas.ny@chasacademy.se>
2020-06-10 13:56:38 +0200

    Add trim to user.model

--- backend/db/models/user.model.js
+++ backend/db/models/user.model.js
@@ -3,38 +3,44 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   adress: {
     type: String,
+    trim: true,
     required: true,
   },
   coAdress: {
     type: String,
+    trim: true,
     required: false,
   },
   email: {
     type: String,
+    trim: true,
     required: true,
   },
   firstName: {
     type: String,
-    required: true,
     trim: true,
     minlength: 2,
+    required: true,
   },
   lastName: {
     type: String,
-    required: true,
     trim: true,
     minlength: 2,
+    required: true,
   },
   password: {
     type: String,
+    trim: true,
     required: true,
   },
   postalAdress: {
     type: String,
+    trim: true,
     required: true,
   },
   postalNumber: {
     type: Number,
+    trim: true,
     required: true,
     minlength: 5,
     maxlength: 5,
